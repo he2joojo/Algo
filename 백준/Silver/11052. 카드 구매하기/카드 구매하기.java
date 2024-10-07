94a2ecc74fc56192ee924dc5b1ea35219e2bb01b
HeeJu Cho <sus6an@naver.com>
2024-10-07 20:24:58 +0900

    [Silver I] Title: 카드 구매하기, Time: 96 ms, Memory: 15680 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int n;
+    static int[] arr;
+    static int[][] dp;
+
+    static void input() throws IOException {
+        n = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        arr = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp = new int[n + 1][n + 1];
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        // 1. 입력 및 초기화
+        input();
+
+        // 2. 로직
+        for (int i = 1; i <= n; i++) { // 카드 팩 순회
+            for (int j = 1; j <= n; j++) { // 카드 개수 순회
+                dp[i][j] = dp[i - 1][j];
+                if (j >= i) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i][j - i] + arr[i]);
+                }
+            }
+        }
+
+        // 3. 출력
+        System.out.println(dp[n][n]);
+    }
+}
